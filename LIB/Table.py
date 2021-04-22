c78e11455cc128cde82453da48befba5ced9a124
Kunal-Shahi <shahikunal436@gmail.com>
2021-04-22 11:07:18 +0530

    Second Commit

--- LIB/Table.py
+++ LIB/Table.py
@@ -1,7 +1,6 @@
-from LMS import app, db
+from LMS import app, db, bcrypt
 from LMS.models import User,Post
 db.create_all(app=app)
-user_1=User(username='admin',email='admin@iitd.ac.in',password='password',pos='Admin')
+user_1=User(username='admin',email='admin@iitd.ac.in',password=bcrypt.generate_password_hash('password').decode('utf-8'),pos='Admin')
 db.session.add(user_1)
 db.session.commit()
-User.query.all()
